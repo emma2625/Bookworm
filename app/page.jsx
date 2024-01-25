2966bc684425fac3044ae76ef139fa19c22e7649
emma2625 <emmanuelodobo10@gmail.com>
2024-01-25 12:04:26 +0100

    Update

--- app/page.jsx
+++ app/page.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <section >
-        <div>
-          <h1>Counter</h1>
-          <h1>0</h1>
-          <button> Count </button>
+    <section className='min-h-dvh  flex justify-center items-center'>
+        <div className='bg-white w-96 text-center p-3 shadow-lg shadow-green-200 rounded'>
+          <h1 className='text-2xl mb-10'>Counter</h1>
+
+          <h1 className='text-3xl font-mono'>0</h1>
+
+          <button className='bg-purple-400 py-2 w-full rounded-full border-none'> Count </button>
         </div>
     </section>
   )
