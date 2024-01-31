7b41f88b0e858b8be181ab622285870524555b4b
emma2625 <emmanuelodobo10@gmail.com>
2024-01-31 16:02:29 +0100

    Nav Update

---
+++ components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='p-5 text-center bg-emerald-300'>
+        &copy; 2024, All Rights Reserved.
+    </div>
+  )
+}
+
+export default Footer
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { faSignIn } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faSignIn } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image";
 import Link from "next/link";
@@ -8,15 +8,14 @@ const Navbar = () => {
   return (
     <nav className="p-2 flex bg-emerald-300 justify-between items-center">
       <Link href={"#"}>
-        <Image 
-          src={"/images/logo.png"}
-          alt="Logo"
-          width={60}
-          height={60}
-        />
+        <Image src={"/images/logo.png"} alt="Logo" width={60} height={60} />
       </Link>
 
-      <ul className="flex gap-5 pr-10">
+      <button className="border p-2 py-1 rounded border-gray-500 text-gray-500 hidden max-sm:block">
+       <FontAwesomeIcon icon={faBars} /> 
+      </button>
+
+      <ul className="flex fixed max-sm:w-full max-sm:h-dvh max-sm:top-0 max-sm:left-0 max-sm:bg-emerald-300 max-sm:flex-col max-sm:justify-center max-sm:items-center max-sm:bg-opacity-70 transition-all max-sm:-translate-x-full sm:static gap-5 pr-10">
         <li>
           <Link href={"#"} className="p-2 ">
             Assets
