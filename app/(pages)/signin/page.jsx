1ddce04799e2a61e6609de8b7c9e3d3a43f9a118
emma2625 <emmanuelodobo10@gmail.com>
2024-02-03 14:04:15 +0100

    Google Sign In

--- app/(pages)/signin/page.jsx
+++ app/(pages)/signin/page.jsx
@@ -1,31 +1,51 @@
-import { faGoogle } from '@fortawesome/free-brands-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React from 'react'
+"use client";
+import { faGoogle } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useState } from "react";
+import { signIn } from "next-auth/react";
+import { faRefresh } from "@fortawesome/free-solid-svg-icons";
 
 const page = () => {
+  const [load, setLoad] = useState(false);
+
+  const handleGoogleSignIn = () => {
+    setLoad(true)
+    signIn("google", { callbackUrl: "/" });
+  };
   return (
     <section>
-        <div className="min-h-dvh flex justify-center items-center">
-            <div className="shadow-lg p-5 rounded-lg">
-                <p className="text-center text-3xl mb-10">
-                    Sign Up For a Free Account
-                </p>
+      <div className="min-h-dvh flex justify-center items-center">
+        <div className="shadow-lg p-5 rounded-lg">
+          <p className="text-center text-3xl mb-10">
+            Sign Up For a Free Account
+          </p>
 
-                <form action="">
-                    <input type="email" placeholder='Enter your Email Address' className='border rounded-lg w-full py-2 px-3 mb-5' />
+          <form action="">
+            <input
+              type="email"
+              placeholder="Enter your Email Address"
+              className="border rounded-lg w-full py-2 px-3 mb-5"
+            />
 
-                    <button className='bg-blue-600 w-full rounded-full py-2 text-white mb-5 '>
-                        Sign Up
-                    </button>
-                </form>
+            <button className="bg-blue-600 w-full rounded-full py-2 text-white mb-5 ">
+              Sign Up
+            </button>
+          </form>
 
-                <button className='bg-emerald-600 py-2 w-full rounded-full text-white'>
-                   <FontAwesomeIcon icon={faGoogle} />  Sign Up with Google 
-                </button>
-            </div>
+          <button
+            onClick={handleGoogleSignIn}
+            disabled={load}
+            className="bg-emerald-600 py-2 w-full rounded-full text-white"
+          >
+            <FontAwesomeIcon icon={faGoogle} /> Sign Up with Google
+            {load && (
+              <FontAwesomeIcon icon={faRefresh} className="animate-spin" />
+            )}
+          </button>
         </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default page
+export default page;
--- app/api/auth/[...nextauth]/options.js
+++ app/api/auth/[...nextauth]/options.js
@@ -1,3 +1,20 @@
+import GoogleProvider from "next-auth/providers/google";
+import GitHubProvider from "next-auth/providers/github";
+
 export const authOptions = {
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT,
+            clientSecret: process.env.GOOGLE_SECRET
+        }),
+
+        GitHubProvider({
+            clientId: "Test",
+            clientSecret: "Test"
+        })
+    ],
     
+    pages: {
+        signIn: "/signin"
+    }
 }
